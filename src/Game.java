571355a1b24fae6e9128c08fcb6d6985d5f396d2
jakubBone <jakub.bone@poczta.onet.pl>
2023-10-20 11:09:10 +0200

    Commit 3: Added static list of standard items and method() adding a new item into the list

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,25 @@
 import java.util.ArrayList;
 
 public class Game {
-    void startGame(){
+    public static ArrayList<String> bag = new ArrayList<>();
+    static{
+        bag.add("knife");
+        bag.add("tequila");
+        bag.add("bottle");
+        bag.add("lighter");
+        bag.add("dictionary");
+    }
+
+    public void itemList(){
+        int i = 0;
+        for(String element: bag){
+            System.out.println(i + " - " + element);
+            i++;
+        }
+        System.out.println();
+    }
+
+    public void addItem(String item){
+        bag.add(item);
     }
 }
--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,7 @@
 public class TestClass {
     public static void main(String[] args) {
         Game game = new Game();
-        game.startGame();
+        game.addItem("poison");
+        game.itemList();
     }
 }

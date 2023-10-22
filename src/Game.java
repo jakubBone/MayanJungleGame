56fb7ce18c33fa249ee7014daec473331aca10bb
jakubBone <jakub.bone@poczta.onet.pl>
2023-10-22 10:16:41 +0200

    Commit 5: Added startGame() with Exception method asking user for name

--- src/Game.java
+++ src/Game.java
@@ -4,12 +4,30 @@ import java.util.Scanner;
 
 public class Game {
 Scanner scanner = new Scanner(System.in);
+
     boolean gameOver;
     void gamePlay() {
+        startGame();
         while (!gameOver) {
             chooseMovement();
             itemList();
             gameOver = true;
+        }
+    }
+
+    void startGame() {
+        String name;
+        while(true) {
+            try {
+                System.out.println("Welcome in game. Please, enter your name");
+                name = scanner.nextLine();
+                if (!name.matches("^[a-zA-Z]+$"))
+                    throw new IllegalArgumentException("The name can't contain numbers or special signs")
+            System.out.println("Hello" + name);
+            break;
+            } catch(IllegalArgumentException ex){
+                System.out.println("Error: " + ex.getMessage());
+            }
         }
     }
 
